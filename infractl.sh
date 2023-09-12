5a46bc08a9c2a35398b7d43cd30321482120dbc1
Andrey Gubarev <andrey@andreygubarev.com>
2023-09-12 13:12:49 +0400

    extract logging

--- infractl.sh
+++ infractl.sh
@@ -9,6 +9,9 @@ INFRACTL_PATH="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
 # shellcheck source=lib/api.sh
 source "$INFRACTL_PATH/lib/api.sh"
 
+# shellcheck source=lib/logging.sh
+source "$INFRACTL_PATH/lib/logging.sh"
+
 ### Plugins ###################################################################
 INFRACTL_PLUGINS_PATH="$INFRACTL_PATH/plugins"
 
@@ -22,62 +25,8 @@ source "$INFRACTL_PLUGINS_PATH/api/terraform/v1alpha1/plugin.sh"
 ### Runtime ###################################################################
 INFRACTL_DRYRUN="${INFRACTL_DRYRUN:-false}"
 
-### Logging ###################################################################
-INFRACTL_LOGLEVEL="${INFRACTL_LOGLEVEL:-warn}"
-
-if [ "${INFRACTL_LOGLEVEL}" == "trace" ]; then
-    set -x
-fi
-
-log() {
-    local loglevel="$1"
-    shift
-
-    case "$INFRACTL_LOGLEVEL" in
-        "trace")
-            level=0
-            ;;
-        "debug")
-            level=10
-            ;;
-        "info")
-            level=20
-            ;;
-        "warn")
-            level=30
-            ;;
-        "error")
-            level=40
-            ;;
-        "critical")
-            level=50
-            ;;
-        *)
-            echo "Invalid log level: $loglevel"
-            exit 1
-            ;;
-    esac
-
-    local message="$*"
-
-    if [ "$loglevel" == "trace" ] && [ "$level" -le 0 ]; then
-        echo "DEBUG: $message" >&2
-    elif [ "$loglevel" == "debug" ] && [ "$level" -le 10 ]; then
-        echo "DEBUG: $message" >&2
-    elif [ "$loglevel" == "info" ] && [ "$level" -le 20 ]; then
-        echo "INFO: $message" >&2
-    elif [ "$loglevel" == "warn" ] && [ "$level" -le 30 ]; then
-        echo "WARNING: $message" >&2
-    elif [ "$loglevel" == "error" ] && [ "$level" -le 40 ]; then
-        echo "ERROR: $message" >&2
-    elif [ "$loglevel" == "critical" ] && [ "$level" -le 50 ]; then
-        echo "CRITICAL: $message" >&2
-        exit 1
-    fi
-}
-
 ### Templates #################################################################
-TEMPLATES_DIR="$(cd "$(dirname "$0")" && pwd)/templates"
+TEMPLATES_DIR="$INFRACTL_PATH/templates"
 
 template_render() {
     log info "rendering template: $1"
