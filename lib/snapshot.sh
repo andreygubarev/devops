1b24f64c6da55eba36da7dd3e78e0fe287fa4082
Andrey Gubarev <andrey@andreygubarev.com>
2023-09-14 02:10:34 +0400

    update snapshot

--- lib/snapshot.sh
+++ lib/snapshot.sh
@@ -6,24 +6,24 @@ snapshot::path() {
 }
 
 snapshot::new() {
-    snapshot_path=$(readlink -f "$1")
+    snapshot_file="$1"
+    snapshot_path=$(readlink -f "$snapshot_file")
     if [ -f "$snapshot_path" ]; then
         snapshot_path=$(dirname "$snapshot_path")
         log debug "snapshot: new: $snapshot_path"
     else
-        log critical "snapshot: not found: $1"
+        log critical "snapshot: not found: $snapshot_file"
     fi
 
     snapshot_archive="$(mktemp)"
-    tar -czf "$snapshot_archive" -C "$snapshot_path" .
+    tar -czf "$snapshot_archive" -C "$snapshot_path" --exclude=".infractl" .
     log debug "snapshot: archive: $snapshot_archive"
 
     snapshot_hash=$(sha1sum "$snapshot_archive" | awk '{print $1}')
     log debug "snapshot: hash: $snapshot_hash"
 
-    snapshot_dir=$(snapshot::path)
-    log debug "snapshot: dir: $snapshot_dir"
+    log debug "snapshot: dir: $(snapshot::path)"
 
-    mkdir -p "$snapshot_dir"
-    tar -xzf "$snapshot_archive" -C "$snapshot_dir"
+    mkdir -p "$(snapshot::path)"
+    tar -xzf "$snapshot_archive" -C "$(snapshot::path)"
 }
